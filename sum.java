0b6644117cdcce0cadc6e0dde409777b6d412a5b
devopswith <puthabheemreddy@gmail.com>
2022-09-16 09:21:22 -0700

    created a copy of add.java

---
+++ sum.java
@@ -0,0 +1,15 @@
+class sum
+{
+	public static void main(string args[])
+	{
+		int a,b,rest;
+		a=10;
+		b=25;
+		res=a+b;
+		system.out.println("sum of 10 & 25 is :"+rest);
+		system.out.prientln("thank you & have a day");
+	}
+	
+	
+}	
+
